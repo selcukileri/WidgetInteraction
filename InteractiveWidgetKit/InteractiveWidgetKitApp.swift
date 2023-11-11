a82baec2f59c0928c67ad8a628e504569ceb7b40
Selçuk İleri <selcukileri98@gmail.com>
2023-11-12 02:19:00 +0300

    Initial Commit

---
+++ InteractiveWidgetKit/InteractiveWidgetKitApp.swift
@@ -0,0 +1,17 @@
+//
+//  InteractiveWidgetKitApp.swift
+//  InteractiveWidgetKit
+//
+//  Created by Selçuk İleri on 12.11.2023.
+//
+
+import SwiftUI
+
+@main
+struct InteractiveWidgetKitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
